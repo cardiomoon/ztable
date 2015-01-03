ed149e7d97b1f183296f3325b028e60a7572a878
cardiomoon <cardiomoon@gmail.com>
2015-01-03 16:37:22 +0900

    version 0.1.2
    CRAN submission

--- R/ztable.R
+++ R/ztable.R
@@ -150,9 +150,9 @@ ztable.data.frame=function(x,digits=NULL,...){
 #' require(ztable)
 #' x=head(iris)
 #' ztable(x)
-#' ztable(x,size=3,caption="Table 1. mylatex Test")
-#' ztable(x,size=7,caption="Table 1. mylatex Test",caption.position="l")
-#' ztable(x,size=7,caption="Table 1. mylatex Test",caption.placement="bottom",
+#' ztable(x,size=3,caption="Table 1. ztable Test")
+#' ztable(x,size=7,caption="Table 1. ztable Test",caption.position="l")
+#' ztable(x,size=7,caption="Table 1. ztable Test",caption.placement="bottom",
 #'       caption.position="l")
 #' fit=lm(mpg~.,data=mtcars)
 #' ztable(fit)
@@ -168,6 +168,8 @@ ztable.data.frame=function(x,digits=NULL,...){
 #' colon$TS = Surv(time,status==1)
 #' out1=coxph(TS~rx+obstruct+adhere+differ+extent+surg+node4,data=colon)
 #' ztable(out1)
+#' ztable(head(mtcars),zebra=1)
+#' ztable(head(mtcars),zebra=1,zebra.type=2)
 ztable_sub=function(x,
                     size=5, # normal size, range 1-10
                     color=getOption("ztable.color","black"),
